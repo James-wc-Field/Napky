3f9bc577aeb2e813d94ecac6559a85d3dac3ff3d
Caden Maxwell <cadenr.maxwell@gmail.com>
2023-11-23 01:37:31 -0700

    need material-UI, not material 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { redirect } from 'next/navigation'
 export default function Home() {
   // If authenticated, redirect to the User discover page
