cb26f0ff9f2e2d366b7b64e8ed6c8560373db129
Caden Maxwell <cadenr.maxwell@gmail.com>
2023-11-23 00:31:25 -0700

    testing loading.tsx functionality

--- src/app/test_loading/loading.tsx
+++ src/app/test_loading/loading.tsx
@@ -1,4 +1,4 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <h1>Loading an accounts page</h1>;
+  return <h1>loading something awesome...</h1>;
 }
