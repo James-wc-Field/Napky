f438cc0115a20d21a90cf67197a259a284a8559d
Caden Maxwell <cadenr.maxwell@gmail.com>
2023-11-22 23:15:10 -0700

    playing around with routing

--- napky/src/app/(accounts)/signup/page.tsx
+++ napky/src/app/(accounts)/signup/page.tsx
@@ -1,3 +1,20 @@
 export default function Page() {
-    return <h1>this is the signup page!!!!!!!!!</h1>
-  }
+  return (
+		<>
+      <h1>this is the signup page!!!</h1>
+			<p>signup page</p>
+
+			<form>
+				<label>email</label>
+				<input type="email" />
+				<br />
+				<label>password</label>
+				<input type="password" />
+				<br />
+				<label>confirm password</label>
+				<input type="password" />
+				<input type="submit" />
+			</form>
+		</>
+  );
+}
