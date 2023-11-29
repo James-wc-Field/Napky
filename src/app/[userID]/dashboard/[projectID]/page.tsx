c111c6c8cb595a19b9b991cc6a5ec270e7bbaddf
Hamilton Hardy <hamilton.j.hardy@gmail.com>
2023-11-29 12:01:57 -0700

    component drag and drop working

--- src/app/[userID]/dashboard/[projectID]/page.tsx
+++ src/app/[userID]/dashboard/[projectID]/page.tsx
@@ -5,17 +5,39 @@ import { useState, useId} from "react";
 import Grid from "../../../components/Grid"; // Update the import path
 import TestDraggable from "../../../components/TestDraggable"; // Update the import path
 import DrawingBoard from "@/app/components/DrawingBoard";
+interface Draggables {
+    [key: string]: Position;
+}
+interface Position {
+    x: number;
+    y: number;
+}
+
 export default function Page({ params }: { params: { projectID: string } }) {
-    const id = useId()
+    const [draggables, setDraggables] = useState<Draggables>({});
+
+    function onDragEnd(event: any) {
+        console.log(event)
+        console.log("Moved!")
+        setDraggables(prev => ({ ...prev, [event.active.id]: { x: prev[event.active.id].x + event.delta.x, y: prev[event.active.id].y + event.delta.y } }));
+    }
+
+    const createNewDraggable = () => {
+        const newId = `draggable-${Object.keys(draggables).length + 1}`;
+        setDraggables(prev => ({ ...prev, [newId]: { x: 20, y: 20 } }));
+    };
+
     return (
-        <DndContext id={id}>
+        <DndContext onDragEnd={onDragEnd}>
             <div>My Project: {params.projectID}</div>
-            <Grid>
+            <button onClick={createNewDraggable}>Add Draggable</button>
                 <DrawingBoard>
-                <TestDraggable id="draggable-1">Drag me!</TestDraggable>
-                <TestDraggable id="draggable-2">Drag me!</TestDraggable>
+                    {Object.entries(draggables).map(([id, position]) => (
+                        <TestDraggable key={id} id={id} position={position} onDragEnd={onDragEnd}>
+                            Drag me!
+                        </TestDraggable>
+                    ))}
                 </DrawingBoard>
-            </Grid>
         </DndContext>
     );
 }
