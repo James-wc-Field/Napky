dd0a857fa4e7326913160199f6ab130946f329ac
Hamilton Hardy <hamilton.j.hardy@gmail.com>
2023-11-29 10:59:10 -0700

    basic drawing board

--- src/app/[userID]/dashboard/[projectID]/page.tsx
+++ src/app/[userID]/dashboard/[projectID]/page.tsx
@@ -1,15 +1,20 @@
 'use client'
 import { DndContext } from "@dnd-kit/core";
 import React from "react";
+import { useState, useId} from "react";
 import Grid from "../../../components/Grid"; // Update the import path
 import TestDraggable from "../../../components/TestDraggable"; // Update the import path
+import DrawingBoard from "@/app/components/DrawingBoard";
 export default function Page({ params }: { params: { projectID: string } }) {
+    const id = useId()
     return (
-        <DndContext>
+        <DndContext id={id}>
             <div>My Project: {params.projectID}</div>
             <Grid>
+                <DrawingBoard>
                 <TestDraggable id="draggable-1">Drag me!</TestDraggable>
                 <TestDraggable id="draggable-2">Drag me!</TestDraggable>
+                </DrawingBoard>
             </Grid>
         </DndContext>
     );
