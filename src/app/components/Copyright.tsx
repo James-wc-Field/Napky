f56e71252948c04bc1d51daa5bb739284af4eda3
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2023-12-08 10:37:44 -0700

    added cursor pointer to links and changed some spacing

--- src/app/components/Copyright.tsx
+++ src/app/components/Copyright.tsx
@@ -1,10 +1,12 @@
-import Link from 'next/link';
+import { Link } from '@nextui-org/link'
 
-export default function Copyright(props: any) {
+export default function Copyright() {
   return (
     <div className="text-center text-gray-500 text-xs">
       &copy; {new Date().getFullYear()}{' '}
-      <Link href="https://github.com/James-wc-Field/Napky">Napky</Link>. All
+      <Link
+        className="text-xs" href="https://github.com/James-wc-Field/Napky">Napky
+      </Link>. All
       rights reserved.
     </div>
   );
--- src/app/components/LoginCard.tsx
+++ src/app/components/LoginCard.tsx
@@ -1,8 +1,13 @@
 "use client"
 import React from "react";
-import {
-	Tabs, Tab, Input, Link, Button, Card, CardBody, CardHeader, CardFooter
-} from "@nextui-org/react";
+import { Tabs, Tab } from "@nextui-org/tabs"
+import { Input } from "@nextui-org/input"
+import { Checkbox } from "@nextui-org/checkbox"
+import { Link } from "@nextui-org/link"
+import { Button } from "@nextui-org/button"
+import { Card, CardBody, CardFooter } from "@nextui-org/card"
+
+import NextLink from "next/link";
 
 import Copyright from "../components/Copyright";
 
@@ -18,7 +23,7 @@ export default function App() {
 						size="md"
 						aria-label="Tabs form"
 						selectedKey={selected}
-						onSelectionChange={setSelected}
+						onSelectionChange={(key: React.Key) => setSelected(key as string)}
 					>
 						<Tab key="login" title="Login">
 							<form className="flex flex-col gap-4">
@@ -29,45 +34,58 @@ export default function App() {
 									placeholder="Enter your password"
 									type="password"
 								/>
-								<p className="text-center text-small">
-									Need to create an account?{" "}
-									<Link size="sm" onPress={() => setSelected("sign-up")}>
-										Sign up
-									</Link>
-								</p>
+								<Checkbox defaultSelected>Remember me</Checkbox>
+
 								<div className="flex gap-2 justify-end">
 									<Button fullWidth color="primary">
 										Login
 									</Button>
+								</div>
+								<div className="flex flex-col gap-1">
+									<div className="flex group gap-2 justify-center">
+										<p className="text-sm text-center">
+											Don't have an account?
+										</p>
+										<Link className="cursor-pointer" size="sm" onPress={() => setSelected("sign-up")}>
+											Sign up
+										</Link>
+									</div>
+									<Link size="sm" href='/forgot-password' className="justify-center cursor-pointer">
+											Forgot password?
+									</Link>
 								</div>
 							</form>
 						</Tab>
 						<Tab key="sign-up" title="Sign up">
 							<form className="flex flex-col gap-4 h-[300px]">
 								<Input isRequired label="Name" placeholder="Enter your name" type="password" />
-								<Input isRequired label="Email" placeholder="Enter your email" type="email" />
+								<Input
+									isRequired
+									label="Email"
+									placeholder="Enter your email"
+									type="email"
+								/>
 								<Input
 									isRequired
 									label="Password"
-									placeholder="Enter your password"
 									type="password"
 								/>
-								<p className="text-center text-small">
-									Already have an account?{" "}
-									<Link size="sm" onPress={() => setSelected("login")}>
-										Login
-									</Link>
-								</p>
 								<div className="flex gap-2 justify-end">
 									<Button fullWidth color="primary">
 										Sign up
 									</Button>
 								</div>
+								<p className="text-center text-small">
+									Already have an account?{" "}
+									<Link size="sm" className="cursor-pointer" onPress={() => setSelected("login")}>
+										Login
+									</Link>
+								</p>
 							</form>
 						</Tab>
 					</Tabs>
 				</CardBody>
-				<CardFooter>
+				<CardFooter className="flex flex-col">
 					<Copyright />
 				</CardFooter>
 			</Card>
