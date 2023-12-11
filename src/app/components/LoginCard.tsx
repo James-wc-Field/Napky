f6a72a72f734ec63c98a2d477ac3cdf69d157c8b
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2023-12-10 22:22:35 -0700

    Eyefilled icons and page centered

--- src/app/components/LoginCard.tsx
+++ src/app/components/LoginCard.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React from "react";
-import { motion } from "framer-motion";
 import { Tabs, Tab } from "@nextui-org/tabs"
 import { Input } from "@nextui-org/input"
 import { Checkbox } from "@nextui-org/checkbox"
@@ -9,13 +8,22 @@ import { Link } from "@nextui-org/link"
 import { Button } from "@nextui-org/button"
 import { Card, CardBody, CardFooter } from "@nextui-org/card"
 
+import { EyeFilledIcon } from "./icons/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "./icons/EyeSlashFilledIcon";
+
 import Copyright from "../components/Copyright";
 
 export default function Page() {
 	const [selected, setSelected] = React.useState("login");
+  const [isVisible, setIsVisible] = React.useState(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
+	function tabChange(key: React.Key) {
+		setIsVisible(false); // Reset password visibility when changing tabs
+		setSelected(key as string);
+	}
 
 	return (
-		<div className="flex flex-col w-full">
+		<>
 			<Card className="max-w-full w-[340px] h-[400px]">
 				<CardBody className="overflow-hidden">
 					<Tabs
@@ -23,16 +31,30 @@ export default function Page() {
 						size="md"
 						aria-label="Tabs form"
 						selectedKey={selected}
-						onSelectionChange={(key: React.Key) => setSelected(key as string)}
+						onSelectionChange={(key: React.Key) => tabChange(key)}
 					>
 						<Tab key="login" title="Login">
 							<form className="flex flex-col gap-4">
-								<Input isRequired label="Email" placeholder="Enter your email" type="email" />
+								<Input
+									isRequired
+									label="Email"
+									type="email"
+									placeholder="Enter your email"
+								/>
 								<Input
 									isRequired
 									label="Password"
 									placeholder="Enter your password"
-									type="password"
+									endContent={
+										<button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+											{isVisible ? (
+												<EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+											) : (
+												<EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+											)}
+										</button>
+									}
+									type={isVisible ? "text" : "password"}
 								/>
 								<Checkbox defaultSelected>Remember me</Checkbox>
 
@@ -46,7 +68,7 @@ export default function Page() {
 										<p className="text-sm text-center">
 											Don't have an account?
 										</p>
-										<Link className="cursor-pointer" size="sm" onPress={() => setSelected("sign-up")}>
+										<Link className="cursor-pointer" size="sm" onPress={() => tabChange("sign-up")}>
 											Sign up
 										</Link>
 									</div>
@@ -58,17 +80,32 @@ export default function Page() {
 						</Tab>
 						<Tab key="sign-up" title="Sign up">
 							<form className="flex flex-col gap-4 h-[300px]">
-								<Input isRequired label="Name" placeholder="Enter your name" type="password" />
+								<Input
+									isRequired
+									label="Name"
+									type="text"
+									placeholder="Enter your name"
+								/>
 								<Input
 									isRequired
 									label="Email"
-									placeholder="Enter your email"
 									type="email"
+									placeholder="Enter your email"
 								/>
 								<Input
 									isRequired
 									label="Password"
-									type="password"
+									endContent={
+										<button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+											{isVisible ? (
+												<EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+											) : (
+												<EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+											)}
+										</button>
+									}
+									type={isVisible ? "text" : "password"}
+									placeholder="Enter your password"
 								/>
 								<div className="flex gap-2 justify-end">
 									<Button fullWidth color="primary">
@@ -77,7 +114,7 @@ export default function Page() {
 								</div>
 								<p className="text-center text-small">
 									Already have an account?{" "}
-									<Link size="sm" className="cursor-pointer" onPress={() => setSelected("login")}>
+									<Link size="sm" className="cursor-pointer" onPress={() => tabChange("login")}>
 										Login
 									</Link>
 								</p>
@@ -89,6 +126,6 @@ export default function Page() {
 					<Copyright />
 				</CardFooter>
 			</Card>
-		</div>
+		</>
 	);
 }
