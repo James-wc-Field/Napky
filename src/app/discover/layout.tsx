73100e6df20890a027a496764be9c0ac129a9894
MasterBowtie <greenducky12@hotmail.com>
2023-12-18 14:14:56 -0700

    Made a Navigation Bar

--- src/app/discover/layout.tsx
+++ src/app/discover/layout.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import { Lusitana, Rubik_Maze } from 'next/font/google';
+
 export default function DashboardLayout({
     children, // will be a page or nested layout
   }: {
@@ -6,7 +9,16 @@ export default function DashboardLayout({
     return (
       <section>
         {/* Include shared UI here e.g. a header or sidebar */}
-        <nav></nav>
+        <div className="bg-main_purple h-fit max-w-full">
+          <Image 
+          src='/icon.png'
+          height={80}
+          width={80}
+          className='ml-5 inline-block'
+          alt='This is the filler Icon'
+          />
+          <span className='inline-block align-bottom pb-4 ml-5 font-extrabold text-5xl'>Napkey</span>
+          </div>
    
         {children}
       </section>
