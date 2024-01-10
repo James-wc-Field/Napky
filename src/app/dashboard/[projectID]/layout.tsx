887733955c5630d09a841ebea93e2aedcd023cd9
James-wc-Field <126519921+James-wc-Field@users.noreply.github.com>
2024-01-10 10:31:48 -0700

    got every thing working together

--- src/app/dashboard/[projectID]/layout.tsx
+++ src/app/dashboard/[projectID]/layout.tsx
@@ -12,30 +12,6 @@ export default function DashboardLayout({
   const [collapsed, setSidebarCollapsed] = useState(false);
   return (
     <section>
-      <Navbar>
-        <NavbarBrand>
-          <p className="font-bold text-inherit">Napky</p>
-        </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-4" justify="center">
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              Dashboard
-            </Link>
-          </NavbarItem>
-          <NavbarItem isActive>
-            <Link href="#" aria-current="page">
-              Discover
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-        <NavbarContent justify="end">
-          <NavbarItem>
-            <Button as={Link} color="primary" href="#" variant="flat">
-              Account
-            </Button>
-          </NavbarItem>
-        </NavbarContent>
-      </Navbar>
       <div
       className={classNames({
         // 👇 use grid layout
