16e5fc936ba7b6550aecc9a13670d2db7493b13b
MasterBowtie <greenducky12@hotmail.com>
2024-01-16 16:02:17 -0700

    designing the card for Discover page

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import React from 'react'
 import DiscoverCard from '../components/DiscoverCard'
+
 export default function Page() {
   const list = [
-    {"username": "MasterBowtie", "project": "Pathfinder", "modified": "01/12/2024", "created": "02/27/2022", "rating":4.2, "image": "public/images/pexels-alina-chernii-19979454.jpg", "image_text": "This is an image"}]
+    {"username": "MasterBowtie", "project": "Pathfinder", "modified": "01/12/2024", "created": "02/27/2022", "rating":4.2, "image": "https://i1.wp.com/nerdsonearth.com/wp-content/uploads/2020/06/20190624-42.jpg?fit=1200%2C776&ssl=1", "image_text": "This is an image"}]
   return (
   <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
   {list.map((item, index) => {
