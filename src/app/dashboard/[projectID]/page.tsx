5f66e8228e9e4e48ee2b00fdd2f4d03fc7d88381
cmax <a02335780@usu.edu>
2024-01-24 13:25:11 -0700

    removed tldraw

--- src/app/dashboard/[projectID]/page.tsx
+++ src/app/dashboard/[projectID]/page.tsx
@@ -1,16 +1,10 @@
-'use client'
+'use client';
 import { DndContext } from "@dnd-kit/core";
-import React from "react";
-import {Tldraw} from '@tldraw/tldraw';
-import { useState } from "react";
-import TestDraggable from "../../components/TestDraggable"; // Update the import path
-import DrawingBoard from "@/app/components/DrawingBoard";
+
 export default function Page({ params }: { params: { projectID: string } }) {
+	return (
+		<>
 
-
-    return (
-        <div style={{position: 'fixed', inset: 0}}>
-            <Tldraw/>
-        </div>  
-    );
+		</>
+	);
 }
