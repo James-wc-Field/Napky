4c53aa612a1e8a84810b7fc259f1b66770aa6421
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2024-01-26 04:01:28 -0700

    add touch, mouse, and some delay

--- src/app/components/ProjectBuilder.tsx
+++ src/app/components/ProjectBuilder.tsx
@@ -4,13 +4,18 @@ import React, { useId } from "react";
 import { Project } from "../builder/SchemaSimulation";
 import SaveProjectBtn from "./SaveProjectBtn";
 import Builder from "./Builder";
-import { DndContext } from "@dnd-kit/core";
+import { DndContext, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
 import DragOverlayWrapper from "./DragOverlayWrapper";
 
 function ProjectBuilder({ project }: { project: Project }) {
   const id = useId();
+  const sensors = useSensors(
+    useSensor(MouseSensor, { activationConstraint: { distance: 5 } }),
+    useSensor(TouchSensor, { activationConstraint: { delay: 100, tolerance: 5 } }),
+  );
+
   return (
-    <DndContext id={id}>
+    <DndContext id={id} sensors={sensors}>
       <main className="flex flex-col w-full">
         <div className="flex justify-between border-b-1 border-slate-500 p-4 gap-3 items-center">
           <h2 className="truncate font-medium">
