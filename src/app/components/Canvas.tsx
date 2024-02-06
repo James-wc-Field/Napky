c4a7a0732706ec98c2200f20be0b3a80c2e15e62
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2024-02-05 23:07:41 -0700

    good zoom behavior without zoom package

--- src/app/components/Canvas.tsx
+++ src/app/components/Canvas.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDraggable, useDroppable } from "@dnd-kit/core";
 import { ProjectElementInstance, ProjectElements } from "./ProjectElements";
-import { Card } from "@nextui-org/react";
+import { Button, Card } from "@nextui-org/react";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { MinusIcon } from "@heroicons/react/24/solid";
-import { TransformComponent, useTransformEffect } from "react-zoom-pan-pinch";
 
 export default function Canvas({
   elements,
@@ -29,21 +28,51 @@ function MainCanvasDroppable({ children }: { children?: React.ReactNode }) {
       isCanvasDropArea: true,
     },
   });
+  const [zoomLevel, setZoomLevel] = useState(1);
+
+  const minScale = 0.2;
+  const maxScale = 3;
+  function handleButtonPress(type: "plus" | "minus") {
+    setZoomLevel((prev) => {
+      if (prev <= minScale && type === "minus") return prev;
+      if (prev >= maxScale && type === "plus") return prev;
+      if (type === "plus") return prev * 1.2;
+      return prev / 1.2;
+    });
+  }
 
   return (
-    <TransformComponent
-      wrapperStyle={{
-        maxWidth: "100%",
-        maxHeight: "100%",
-        position: "absolute",
-      }}
-    >
-      <div id="canvas-drop-area" ref={setNodeRef} className="bg-white/20">
-        <div className="w-[10000px] h-[10000px] dark:bg-[url(/dark-paper.svg)]">
-          {children}
+    <>
+      <div className="absolute inset-0 overflow-auto flex items-center">
+        <div
+          className="m-auto"
+          style={{
+            width: `calc(3000px * ${zoomLevel})`,
+            height: `calc(3000px * ${zoomLevel})`,
+          }}
+        >
+          <div
+            className="w-[3000px] h-[3000px] dark:bg-[url(/dark-paper.svg)]"
+            style={{
+              transform: `scale(${zoomLevel})`,
+              transformOrigin: "top left",
+            }}
+          >
+            <div id="canvas-drop-area" ref={setNodeRef} className="bg-white/20 w-full h-full">
+              {children}
+            </div>
+          </div>
         </div>
       </div>
-    </TransformComponent>
+      <Card className="absolute top-4 right-4 gap-2 p-2">
+        <Button onPress={() => handleButtonPress("plus")}>
+          <PlusIcon className="w-6" />
+        </Button>
+        <Button onPress={() => handleButtonPress("minus")}>
+          <MinusIcon className="w-6" />
+        </Button>
+      </Card>
+    </>
   );
 }
 
