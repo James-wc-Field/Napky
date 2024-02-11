7c480f28ab882ddcde7e14eff1c1b366dfd78e78
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2024-02-11 04:15:08 -0700

    Now thats nice

--- src/app/components/Canvas.tsx
+++ src/app/components/Canvas.tsx
@@ -1,10 +1,10 @@
-import { ReactNode, useRef } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import { useDraggable, useDroppable } from "@dnd-kit/core";
 import { ProjectElementInstance, ProjectElements } from "./ProjectElements";
-import { Button, Card } from "@nextui-org/react";
-import { PlusIcon } from "@heroicons/react/24/solid";
-import { MinusIcon } from "@heroicons/react/24/solid";
 import useProject from "./hooks/useProject";
+import MiniMap from "./MiniMap";
+import CanvasControls from "./CanvasControls";
+import CanvasBackground from "./CanvasBackground";
 
 export default function Canvas({
   elements,
@@ -36,37 +36,39 @@ function MainCanvasDroppable({ children }: { children?: ReactNode }) {
     scrollLeft,
     updateScrollLeft,
     updateScrollTop,
-    elements,
+    updateCanvasViewRect,
   } = useProject();
-
-  const minScale = 0.05;
-  const maxScale = 5;
-  function handleButtonPress(type: "plus" | "minus") {
-    updateZoomLevel((prev) => {
-      if (prev / 1.2 <= minScale && type === "minus") return prev;
-      if (prev * 1.2 >= maxScale && type === "plus") return prev;
-      if (type === "plus") return prev * 1.2;
-      return prev / 1.2;
-    });
-  }
 
   function handleScroll(e: React.WheelEvent<HTMLDivElement>) {
     const { deltaX, deltaY } = e;
     if (e.ctrlKey) {
-      updateZoomLevel((prev) => {
-        if (prev / 1.05 <= minScale && deltaY > 0) return prev;
-        if (prev * 1.05 >= maxScale && deltaY < 0) return prev;
-        if (deltaY > 0) return prev / 1.05;
-        return prev * 1.05;
-      });
+      updateZoomLevel(deltaY > 0, 1.05);
+      return;
+    } else if (e.shiftKey) {
+      updateScrollLeft(deltaY);
       return;
     }
-    updateScrollLeft((prev) => prev - deltaX);
-    updateScrollTop((prev) => prev - deltaY);
+    updateScrollLeft(deltaX);
+    updateScrollTop(deltaY);
   }
 
-  // keep canvas-viewport size for minimap
-  const ref = useRef<HTMLDivElement>(null);
+  const canvasViewRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleResize = () => {
+      const { current } = canvasViewRef;
+
+      if (current) {
+        const boundingBox = current.getBoundingClientRect();
+        updateCanvasViewRect(boundingBox);
+      }
+    };
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [canvasViewRef]);
 
   return (
     <>
@@ -77,6 +79,7 @@ function MainCanvasDroppable({ children }: { children?: ReactNode }) {
           zIndex: 4,
         }}
         onWheel={handleScroll}
+        ref={canvasViewRef}
       >
         <div
           id="canvas-pane-droppable"
@@ -92,95 +95,14 @@ function MainCanvasDroppable({ children }: { children?: ReactNode }) {
               transformOrigin: "top left",
               zIndex: 2,
             }}
-            ref={ref}
           >
             {children}
           </div>
         </div>
       </div>
-
-      {/* Canvas Controls */}
-      <Card className="absolute top-4 right-4 gap-2 p-2" style={{ zIndex: 5 }}>
-        <Button
-          isIconOnly
-          variant="flat"
-          onPress={() => handleButtonPress("plus")}
-        >
-          <PlusIcon className="w-6" />
-        </Button>
-        <Button
-          isIconOnly
-          variant="flat"
-          onPress={() => handleButtonPress("minus")}
-        >
-          <MinusIcon className="w-6" />
-        </Button>
-      </Card>
-
-      {/* Minimap to see a small version of the viewport and its elements */}
-      <div
-        className="absolute right-0 bottom-0 bg-neutral-400/50 m-4 rounded-md border-1 border-neutral-500/50"
-        style={{ zIndex: 5 }}
-      >
-        <svg
-          className="w-48 h-32  bg-white/20"
-          viewBox={`
-            0 0
-            ${ref.current?.clientWidth} ${ref.current?.clientHeight}
-          `}
-        >
-          {elements.map((element) => {
-            return (
-              <rect
-                x={element.position.x * zoomLevel + scrollLeft}
-                y={element.position.y * zoomLevel + scrollTop}
-                width={element.size.width * zoomLevel}
-                height={element.size.height * zoomLevel}
-                fill="gray"
-              />
-            );
-          })}
-          <path
-            d={`M0 0 H${ref.current?.clientWidth} V${ref.current?.clientHeight} H0 Z`}
-            fill="rgba(240,240,240,0.3)"
-            fill-rule="evenodd"
-            stroke="white"
-            strokeWidth="1"
-          />
-        </svg>
-      </div>
-
-      {/* Background */}
-      <svg
-        className="absolute w-full h-full top-0 left-0"
-        style={{
-          visibility: zoomLevel < 0.5 ? "hidden" : "visible",
-        }}
-      >
-        <pattern
-          id="background"
-          x={scrollLeft % (24 * zoomLevel)}
-          y={scrollTop % (24 * zoomLevel)}
-          width={24 * zoomLevel}
-          height={24 * zoomLevel}
-          patternUnits="userSpaceOnUse"
-          patternTransform={`translate(${zoomLevel}, ${zoomLevel})`}
-        >
-          <circle
-            cx={zoomLevel}
-            cy={zoomLevel}
-            r={zoomLevel}
-            fill="#91919a"
-          ></circle>
-        </pattern>
-        <rect
-          x="0"
-          y="0"
-          width="100%"
-          height="100%"
-          fill="url(#background)"
-        ></rect>
-      </svg>
+      <CanvasControls />
+      <MiniMap />
+      <CanvasBackground />
     </>
   );
 }
