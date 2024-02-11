2618589da5a18eec49340a027d7733fbb2d02de5
cmax <a02335780@usu.edu>
2024-02-11 04:15:08 -0700

    Now thats nice

---
+++ src/app/components/CanvasBackground.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import useProject from "./hooks/useProject";
+
+export default function CanvasBackground() {
+  const { zoomLevel, scrollLeft, scrollTop } = useProject();
+  const BASE_SIZE = 24;
+  return (
+    <svg
+      className="absolute w-full h-full top-0 left-0"
+      style={{
+        visibility: zoomLevel < 0.5 ? "hidden" : "visible",
+      }}
+    >
+      <pattern
+        id="background"
+        x={scrollLeft % (BASE_SIZE * zoomLevel)}
+        y={scrollTop % (BASE_SIZE * zoomLevel)}
+        width={BASE_SIZE * zoomLevel}
+        height={BASE_SIZE * zoomLevel}
+        patternUnits="userSpaceOnUse"
+        patternTransform={`translate(${zoomLevel}, ${zoomLevel})`}
+      >
+        <circle
+          cx={zoomLevel}
+          cy={zoomLevel}
+          r={zoomLevel}
+          fill="#91919a"
+        ></circle>
+      </pattern>
+      <rect
+        x="0"
+        y="0"
+        width="100%"
+        height="100%"
+        fill="url(#background)"
+      ></rect>
+    </svg>
+  );
+}
