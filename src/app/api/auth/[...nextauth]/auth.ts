7f6161ffad27035258eef5b7693e4a49769b0ff9
jseffker <jseffker@gmail.com>
2024-02-16 00:18:11 -0700

    OAuth for Google working
    It's working now, however, you will need the env.local variables which I can give everyone sometime. Once you have that, go to the /login route and click "Continue with Google". Once you're signed in, in will reroute you back to the login page but show your account at the top. You can sign out by clicking the sign out button in the Test Auth UI section.

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,31 +1,50 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import GitHub from "next-auth/providers/github"
 import EmailProvider from 'next-auth/providers/nodemailer';
 
+// import Auth from "@auth/core"
+// import { default as GoogleAuth } from "@auth/core/providers/google"
+
 export const {
-  handlers: { GET, POST },
+  handlers,
   auth,
 } = NextAuth({
     providers: [
         Google,
-        EmailProvider({
-          server: {
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            auth: {
-              user: process.env.SMTP_USER,
-              pass: process.env.SMTP_PASSWORD,
-            },
-          },
-          from: process.env.SMTP_FROM,
-        }),
+        GitHub,
+      //   EmailProvider({
+      //     server: {
+      //       host: process.env.SMTP_HOST,
+      //       port: process.env.SMTP_PORT,
+      //       auth: {
+      //         user: process.env.SMTP_USER,
+      //         pass: process.env.SMTP_PASSWORD,
+      //       },
+      //     },
+      //     from: process.env.SMTP_FROM,
+      //   }),
       ],
-    //   adapter: MongoDBAdapter(mongoClientPromise, {
-    //     databaseName: process.env.ENVIRONMENT,
-    //   }),
+      // adapter: MongoDBAdapter(mongoClientPromise, {
+      //   databaseName: process.env.ENVIRONMENT,
+      // }),
       pages: {
         signIn: '/auth',
         signOut: '/auth',
       },
     });
+
+// const request = new Request(origin)
+// const response = await Auth(request, {
+//   providers: [Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })],
+// })
+
+// export default NextAuth({
+//   providers: [
+//     Google({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//   ],
+// })
     
