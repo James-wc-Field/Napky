29a08dba44cf6e2b5c72e25abb7c904ea64cdd69
Caden Maxwell <cadenr.maxwell@gmail.com>
2024-02-29 02:54:14 -0700

    Checkboxes working, still need input

--- src/app/components/blocks/TodoBlock.tsx
+++ src/app/components/blocks/TodoBlock.tsx
@@ -7,18 +7,17 @@ import {
   ProjectElementInstance,
 } from "../ProjectElements";
 import { Card } from "@nextui-org/card";
-import { Input } from "@nextui-org/input";
-import { Checkbox, CheckboxGroup } from "@nextui-org/react";
+import { Checkbox, CheckboxGroup, Textarea } from "@nextui-org/react";
 import useProject from "../hooks/useProject";
+import { FormEvent, useEffect } from "react";
 
 const type: ElementsType = "TodoBlock";
 
 const extraAttributes = {
   label: "Todo Block",
   placeHolder: "Enter a task...",
-  text: "",
-  items: [] as string[],
-  checked: [] as boolean[],
+  items: [""] as string[],
+  checked: [false] as boolean[],
 };
 
 export const TodoBlockProjectElement: ProjectElement = {
@@ -52,33 +51,77 @@ function CanvasComponent({
 }) {
   const { updateElement } = useProject();
   const element = elementInstance as CustomInstance;
-  const { placeHolder, text } = element.extraAttributes;
+  const { placeHolder, checked, items } = element.extraAttributes;
   const style = {
     maxWidth: element.size.width,
   };
 
-  function handleOnTextChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleOnAddItem() {
     updateElement(element.id, {
       ...element,
       extraAttributes: {
         ...element.extraAttributes,
-        text: e.target.value,
+        items: [...items, ""],
+        checked: [...checked, false],
       },
     });
   }
 
+  function handleOnTodoItemChange(e: FormEvent<HTMLElement> | string[]) {
+    if (Array.isArray(e)) {
+      handleOnCheckboxChange(e);
+    } else {
+      handleOnTextChange(e);
+    }
+  }
+
+  function handleOnCheckboxChange(e: string[]) {
+    const newChecked = items.map((_, index) => e.includes(index + ""));
+    updateElement(element.id, {
+      ...element,
+      extraAttributes: {
+        ...element.extraAttributes,
+        checked: newChecked,
+      },
+    });
+  }
+
+  function handleOnTextChange(e: FormEvent<HTMLElement>) {
+    const { value } = e.target as HTMLInputElement;
+    console.log(e);
+    const index = parseInt(
+      (e.target as HTMLInputElement).getAttribute("data-index")!
+    );
+    console.log(index, value);
+  }
+
   return (
-    <Card style={style} className="p-2 h-fit">
-      <CheckboxGroup>
-        <div className="flex flex-row grow gap-1">
-          <Checkbox />
-          <Input
-            size="sm"
-            placeholder={placeHolder}
-            onChange={handleOnTextChange}
-            value={text}
-          />
-        </div>
+    <Card
+      style={style}
+      className="p-2 h-fit"
+      onKeyDown={(e) => {
+        if (e.key === "Enter" && e.shiftKey === false) {
+          handleOnAddItem();
+          e.preventDefault();
+        }
+      }}
+    >
+      <CheckboxGroup
+        label={element.extraAttributes.label}
+        onChange={handleOnTodoItemChange}
+        value={checked.map((_, index) => {
+          if (checked[index]) {
+            return index + "";
+          }
+          return "";
+        })}
+      >
+        {items.map((item, index) => (
+          <div key={index} className="flex items-center">
+            <Checkbox value={index + ""} />
+            <Textarea minRows={1} value={item} placeholder={placeHolder} />
+          </div>
+        ))}
       </CheckboxGroup>
     </Card>
   );
