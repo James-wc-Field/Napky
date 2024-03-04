17a62fc12a094e7ce114cb3a930da4a2069730a4
Hamilton Hardy <hamilton.j.hardy@gmail.com>
2024-03-04 16:58:07 -0700

    rebase

--- src/app/components/ProjectBuilder.tsx
+++ src/app/components/ProjectBuilder.tsx
@@ -11,20 +11,37 @@ import {
 } from "@dnd-kit/core";
 
 import { Project } from "../../API";
-import SaveProjectBtn from "./SaveProjectBtn";
 import BuildArea from "./BuildArea";
 import DragOverlayWrapper from "./DragOverlayWrapper";
 import usePreventZoom from "./hooks/usePreventZoom";
+import { Button } from "@nextui-org/react";
+import { DocumentCheckIcon } from "@heroicons/react/24/solid";
+import useProject from "./hooks/useProject";
+import { createProject } from "../../graphql/mutations";
+import { generateClient } from "aws-amplify/api";
+import config from '../../amplifyconfiguration.json';
+import { Amplify } from 'aws-amplify';
+import { ProjectElementInstance } from "./ProjectElements";
 
-export default function ProjectBuilder({ project }: { project: Project }) {
+function ProjectBuilder({ project, saveProject}: { project: Project, saveProject: (elements: ProjectElementInstance[]) => void}) {
+  const {addElements} = useProject();
+  useEffect(() => {
+    if (project) {
+      addElements(JSON.parse(project.content || ""));
+    }
+  }, [project]);
   const id = useId();
+  const {
+    elements
+  } = useProject();
+  console.log(project)
   const sensors = useSensors(
     useSensor(PointerSensor, { activationConstraint: { distance: 2 } }),
     useSensor(TouchSensor, {
       activationConstraint: { delay: 100, tolerance: 5 },
     })
   );
-
+  Amplify.configure(config);
   usePreventZoom();
   return (
     <DndContext id={id} sensors={sensors} collisionDetection={pointerWithin}>
@@ -32,9 +49,12 @@ export default function ProjectBuilder({ project }: { project: Project }) {
         <div className="flex justify-between border-b-1 border-slate-500 p-2 gap-2 items-center">
           <h2 className="truncate font-medium">
             <span className="mr-2">Project:</span>
-            {project.name}
+            {project?.name || "Untitled"}
           </h2>
-          <SaveProjectBtn />
+          <Button onPress={() => saveProject(elements)}>
+            <DocumentCheckIcon className="h-5 w-6" />
+            Save
+          </Button>
         </div>
         <BuildArea />
       </main>
--- src/app/project/[projectID]/page.tsx
+++ src/app/project/[projectID]/page.tsx
@@ -4,7 +4,11 @@ import { generateClient } from 'aws-amplify/api';
 import { getProject } from "@/graphql/queries";
 import { Suspense } from "react";
 import { Project } from "@/API";
-
+import config from '../../../amplifyconfiguration.json'
+import { Amplify } from 'aws-amplify';
+import { getCurrentUser } from "aws-amplify/auth";
+import { createProject } from "@/graphql/mutations";
+import { ProjectElementInstance } from "@/app/components/ProjectElements";
 async function BuilderPage({ params }: { params: { projectID: string } }) {
   const projectID = params.projectID;
   return (
@@ -13,19 +17,37 @@ async function BuilderPage({ params }: { params: { projectID: string } }) {
     </Suspense>
   )
 }
+async function saveProject(elements: ProjectElementInstance[]){
+  'use server'
+  const client = generateClient();
+  await client.graphql({
+    query: createProject,
+    variables: {
+      input: {
+        userId: "user",
+        name: "name",
+        description: "description",
+        content: JSON.stringify(elements)
+      }
+    }
+  });
+}
 
 async function Project({ projectID }: { projectID: string }) {
+  Amplify.configure(config);
+  // const {userId} = await getCurrentUser();
   const client = generateClient();
+  console.log(projectID);
   const project = (await client.graphql({
     query: getProject,
     variables: { id: projectID }
   })).data.getProject;
+  console.log(project);
   return (
     <div>
-      <ProjectBuilder project={project as Project} />
+      <ProjectBuilder project={project as Project} saveProject={saveProject}/>
     </div>
   )
 }
-
 
 export default BuilderPage;
