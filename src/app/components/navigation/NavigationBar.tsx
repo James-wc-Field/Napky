47de135a0455f52691008389035492e3f16b45ea
MasterBowtie <greenducky12@hotmail.com>
2024-03-05 15:12:10 -0700

    attempting to phase out NextUI

--- src/app/components/navigation/NavigationBar.tsx
+++ src/app/components/navigation/NavigationBar.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Link , Image } from "@nextui-org/react";
 import { ReactElement, useEffect, useState } from "react";
 import { usePathname, useRouter } from 'next/navigation'
 import { NavDiscover } from "./NavDiscover";
@@ -22,85 +21,49 @@ export function NavigationBar () {
 
     if (pathname === "/discover") {
         setMiddleState(NavDiscover());
-        setEndState(<Link className="mx-4" color="foreground" href="/dashboard">Dashboard</Link>);
+        setEndState(<a className="mx-4" color="foreground" href="/dashboard">Dashboard</a>);
     }
     else if (pathname === "/dashboard") {
         setMiddleState(NavDashboard());
-        setEndState(<Link className="mx-4" color="foreground" href="/discover">Discover</Link>);
-
+        setEndState(<a className="mx-4" color="foreground" href="/discover">Discover</a>);
     }
     else if (pathname.startsWith("/project/")) {
         setMiddleState(<>{project}</>);
         setEndState(
           <>
-          <Link className="mx-4" color="foreground" href="/discover">Discover</Link>
-          <Link className="mx-4" color="foreground" href="/dashboard">Dashboard</Link>
+          <a className="mx-4" color="foreground" href="/discover">Discover</a>
+          <a className="mx-4" color="foreground" href="/dashboard">Dashboard</a>
           </>);
         }
       },[])
 
   return (
     <>
-    <nav className="bg-content2 flex p-2 items-center font-main px-10">
-      <Link 
-      className="flex justify-center items-center ml-2 min-w-fit"
-      color="foreground"
-      href="/discover"
-      >
-        <Image
+    <nav className="bg-content2-light dark:bg-content2-dark flex p-2 items-center font-main px-10">
+      <a href="/discover" className="flex justify-center items-center ml-2 min-w-fit">
+        <img
         // Need to resolve this issue with project
         className="min-w-12"
         width={48}
+        height={48}
         alt="Happy Squirrel"
         src="images/icon.png"
         />
         <p className="mx-2 text-xl">
           KorkBored
         </p>
-      </Link>
+      </a>
       <div className="flex w-full justify-center"> 
       {middleState}
       </div>
       <div className="flex justify-end min-w-fit">
         {endState}
         {user? 
-          <Link className="mx-4" color="foreground" href="#">Account</Link> : 
-          <Link className="mx-4" color="foreground" href="#">Sign In</Link>}
+          <a className="mx-4" color="foreground" href="#">Account</a> : 
+          <a className="mx-4" color="foreground" href="#">Sign In</a>}
       </div>
     </nav>
     </>
-    // <Navbar position="static" className="bg-content2">
-    //   <NavbarBrand>
-    //     {/* image here  */}
-    //     <p className="font-bold text-inherit">KorkBored</p>
-    //   </NavbarBrand>
-    //   <NavbarContent className="hidden sm:flex gap-4" justify="center">
-    //     <NavbarItem>
-    //       <Link color="foreground" href="#">
-    //         Features
-    //       </Link>
-    //     </NavbarItem>
-    //     <NavbarItem isActive>
-    //       <Link href="#" aria-current="page">
-    //         Customers
-    //       </Link>
-    //     </NavbarItem>
-    //     <NavbarItem>
-    //       <Link color="foreground" href="#">
-    //         Integrations
-    //       </Link>
-    //     </NavbarItem>
-    //   </NavbarContent>
-    //   <NavbarContent justify="end">
-    //     <NavbarItem className="hidden lg:flex">
-    //       <Link href="#">Login</Link>
-    //     </NavbarItem>
-    //     <NavbarItem>
-    //       <Link color="primary" href="#">
-    //         Sign Up
-    //       </Link>
-    //     </NavbarItem>
-    //   </NavbarContent>
-    // </Navbar>
+// shadcn.com UI Framework
   );
 }
