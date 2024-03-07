a92288afdaf557ef1556f9a731b04ecc93d27f03
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2024-03-07 10:10:12 -0700

    forgot password -> Shadcn

--- src/app/(accounts)/login/page.tsx
+++ src/app/(accounts)/login/page.tsx
@@ -2,7 +2,6 @@ import LoginCard from '../../components/LoginCard';
 
 export default function Page() {
   return (
-    // Center login card in page
     <div className="flex h-screen justify-center items-center">
       <LoginCard />
     </div>
