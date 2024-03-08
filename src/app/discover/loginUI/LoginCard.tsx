73c9906c9a84ac2ce2e6c97d0a6d9aab79e6bc0b
jseffker <jseffker@gmail.com>
2024-03-08 13:23:36 -0700

    Login card mostly done

--- src/app/discover/loginUI/LoginCard.tsx
+++ src/app/discover/loginUI/LoginCard.tsx
@@ -1,22 +1,26 @@
+import XIcon from "./XIcon";
 import { Input } from "./input";
 
 
 export default function LoginCard() {
     return (
     <>
-        <div className="w-1/3 min-w-[300px] flex-col p-8 border rounded-[2rem]">
-            <p className="mb-8 text-2xl">Welcome to Napky</p>
+        <div className="max-w-[500px] min-w-[300px] flex-col p-8 border-4 rounded-[2rem] relative">
+            <div className="absolute right-[-1em] top-[-1em] overflow-clip border-4 rounded-full p-2 bg-green-500">
+                <XIcon />
+            </div>
+            <p className="mb-8 text-2xl font-bold">Welcome to Napky</p>
             <div className="flex-col mb-4">
-                <Input type="email" placeholder="email" className="mb-2"></Input>
+                <Input type="email" placeholder="email" className="mb-4"></Input>
                 <div className="flex-col">
-                    <Input type="password" placeholder="password" className="m-2"></Input>
-                    <p className="mt-2 text-xs">Forgot your password?</p>
+                    <Input type="password" placeholder="password" className="mb-2"></Input>
+                    <p className="text-xs">Forgot your password?</p>
                 </div>
             </div>
-            <div className="flex-col items-center px-6">
-                <div className="mb-2 p-2 border rounded-2xl">Log in</div>
-                <p className="w-fit">Or</p>
-                <div className="p-2 border rounded-2xl">Create Account</div>
+            <div className="flex flex-col items-center">
+                <div className="min-w-[50%] text-center mb-2 p-2 rounded-2xl bg-green-500">Log in</div>
+                <p className="w-fit mb-2 self-center">Or</p>
+                <div className="min-w-[50%] text-center mb-2 p-2 rounded-2xl bg-green-500">Create Account</div>
             </div>
         </div>
     </>
