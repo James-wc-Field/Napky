50b6bdb1a3df71abdb7d2200caa5ac574f962e15
MasterBowtie <greenducky12@hotmail.com>
2024-03-08 13:38:41 -0700

    Combined shads

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,17 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{ts,tsx,js,jsx}"],
   prefix: "",
-import type { Config } from "tailwindcss"
-
-const config = {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-  prefix: "",
   theme: {
     container: {
       center: true,
