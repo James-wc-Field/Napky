11f59f129aa8a8714470a3f306a488ad18585204
Hamilton Hardy <hamilton.j.hardy@gmail.com>
2024-03-08 14:13:15 -0700

    move files back

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,7 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-import CanvasContextProvider from "./project/[projectID]/components/context/CanvasContext";
+import CanvasContextProvider from "./components/context/CanvasContext";
 export function Providers({ children }: { children: React.ReactNode }) {
 
   const router = useRouter();
