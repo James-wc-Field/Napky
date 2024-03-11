d056e4cae0521b671c1221fa51f61e0756daf879
cmax <a02335780@usu.edu>
2024-03-11 00:49:50 -0600

    login -> signin for consistency

--- src/app/components/navigation/NavigationBar.tsx
+++ src/app/components/navigation/NavigationBar.tsx
@@ -60,7 +60,7 @@ export function NavigationBar () {
         {endState}
         {user? 
           <a className="mx-4" href="#">Account</a> : 
-          <a className="mx-4" href="/login">Login</a>}
+          <a className="mx-4" href="/sign-in">Sign in</a>}
       </div>
       <ThemeToggle></ThemeToggle>
     </nav>
