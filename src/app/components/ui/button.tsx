9721b980eb51bc9a7b2e110fa35786fa4cc09ee9
MasterBowtie <greenducky12@hotmail.com>
2024-03-21 14:30:27 -0600

    Minor adjustments

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -13,10 +13,10 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-accent bg-background hover:bg-accent hover:text-input",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-hover hover:text-hover-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
