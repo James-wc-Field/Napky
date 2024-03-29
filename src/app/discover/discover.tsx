e81effcf00cb4d58b2a9b519e0392772b26bb6d8
MasterBowtie <greenducky12@hotmail.com>
2024-03-29 13:36:34 -0600

    variable project heights

--- src/app/discover/discover.tsx
+++ src/app/discover/discover.tsx
@@ -16,15 +16,46 @@ type props = {
 export default function DiscoverPage(props: props) {
   const { projects } = props;
   // stuff
-    return (
+  let column_count = 5;
+  let project_Columns: Array<Array<Project>> = [];
+  for (let col = 0; col < column_count; col++) {
+    let column: Array<Project> = []
+    project_Columns.push(column)
+  }
+  
+  // This sorts the projects in the project list into the columns
+  let curr_col = 0;
+  for (let project of projects) {
+    project_Columns[curr_col % column_count].push(project);
+    curr_col++;
+  } 
+ 
+  return (
       <div className='h-base'>
       <ScrollArea className='h-full'>
         <p>Title Here? Welcome Banner?</p>
         <Suspense fallback={<p>Loading...</p>}>
-          <div className="flex flex-wrap gap-7 p-10">
-            {projects.map((project) => (
-              <ProjectCard key={project.id} project={project}/>
-              ))}
+          <div className="flex justify-center gap-3 p-10">
+          {
+          project_Columns.map((column)=> {
+            return (
+              // these are the columns
+              <div className="w-[18vw]">
+                {
+                  column.map((project: any) => {
+                    return (
+                      <div className={`w-full mb-8 bg-white`}>
+                        <ProjectCard project={project}/>
+                      </div>
+                    )
+                  })
+                }
+
+              </div>
+            )
+
+          })
+        }
           </div>
         </Suspense>        
       </ScrollArea>
