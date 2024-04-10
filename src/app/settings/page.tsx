95376a6554a93905b357b1ddc7cd39ed32d6f4fe
jseffker <jseffker@gmail.com>
2024-04-09 18:17:20 -0600

    Here's a start to the settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Suspense } from "react";
 import { LoadingSpinner } from "@/components/ui/spinner";
+import LoginCard from "./_loginUI/LoginCard";
 
 export default function Page() {
   return (
     <Suspense fallback={<Loading />}>
+      <LoginCard></LoginCard>
     </Suspense>
   );
 }
