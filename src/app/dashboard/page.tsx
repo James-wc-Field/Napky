93a242d2a3015d49ee47fdfa4541fe947eda2616
Hamilton Hardy <hamilton.j.hardy@gmail.com>
2024-04-10 11:46:55 -0600

    all middleware changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,9 @@
 import { Suspense } from "react";
 import DashboardPage from "./dashboard";
 import { getAllUserProjects } from "./api";
-import { Button } from "@ui/button";
-import Link from "next/link";
 import { Project } from "@src/API";
-import { currentAuthenticatedUser } from "@/lib/auth";
 
 export default async function Page() {
-  const currentUser = await currentAuthenticatedUser();
-  if (!currentUser)
-    return (
-      <>
-        <p>Not authenticated</p>
-        <Button asChild>
-          <Link href="/sign-in">Sign in</Link>
-        </Button>
-      </>
-    );
-
   const projects: Project[] = await getAllUserProjects();
 
   return (
