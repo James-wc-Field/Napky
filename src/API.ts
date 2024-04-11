0e20bf5376edf21691e65db8e3299f8937540b98
cmax <cadenr.maxwell@gmail.com>
2024-04-11 00:48:45 -0600

    middleware working....?

---
+++ src/API.ts
@@ -0,0 +1,485 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+
+export type CreateCommentsInput = {
+  id?: string | null,
+  comment?: string | null,
+};
+
+export type ModelCommentsConditionInput = {
+  comment?: ModelStringInput | null,
+  and?: Array< ModelCommentsConditionInput | null > | null,
+  or?: Array< ModelCommentsConditionInput | null > | null,
+  not?: ModelCommentsConditionInput | null,
+  createdAt?: ModelStringInput | null,
+  updatedAt?: ModelStringInput | null,
+};
+
+export type ModelStringInput = {
+  ne?: string | null,
+  eq?: string | null,
+  le?: string | null,
+  lt?: string | null,
+  ge?: string | null,
+  gt?: string | null,
+  contains?: string | null,
+  notContains?: string | null,
+  between?: Array< string | null > | null,
+  beginsWith?: string | null,
+  attributeExists?: boolean | null,
+  attributeType?: ModelAttributeTypes | null,
+  size?: ModelSizeInput | null,
+};
+
+export enum ModelAttributeTypes {
+  binary = "binary",
+  binarySet = "binarySet",
+  bool = "bool",
+  list = "list",
+  map = "map",
+  number = "number",
+  numberSet = "numberSet",
+  string = "string",
+  stringSet = "stringSet",
+  _null = "_null",
+}
+
+
+export type ModelSizeInput = {
+  ne?: number | null,
+  eq?: number | null,
+  le?: number | null,
+  lt?: number | null,
+  ge?: number | null,
+  gt?: number | null,
+  between?: Array< number | null > | null,
+};
+
+export type Comments = {
+  __typename: "Comments",
+  id: string,
+  comment?: string | null,
+  createdAt: string,
+  updatedAt: string,
+};
+
+export type UpdateCommentsInput = {
+  id: string,
+  comment?: string | null,
+};
+
+export type DeleteCommentsInput = {
+  id: string,
+};
+
+export type CreateProjectInput = {
+  id?: string | null,
+  userId: string,
+  name: string,
+  description?: string | null,
+  content?: string | null,
+};
+
+export type ModelProjectConditionInput = {
+  userId?: ModelStringInput | null,
+  name?: ModelStringInput | null,
+  description?: ModelStringInput | null,
+  content?: ModelStringInput | null,
+  and?: Array< ModelProjectConditionInput | null > | null,
+  or?: Array< ModelProjectConditionInput | null > | null,
+  not?: ModelProjectConditionInput | null,
+  createdAt?: ModelStringInput | null,
+  updatedAt?: ModelStringInput | null,
+};
+
+export type Project = {
+  __typename: "Project",
+  id: string,
+  userId: string,
+  name: string,
+  description?: string | null,
+  content?: string | null,
+  createdAt: string,
+  updatedAt: string,
+};
+
+export type UpdateProjectInput = {
+  id: string,
+  userId?: string | null,
+  name?: string | null,
+  description?: string | null,
+  content?: string | null,
+};
+
+export type DeleteProjectInput = {
+  id: string,
+};
+
+export type ModelCommentsFilterInput = {
+  id?: ModelIDInput | null,
+  comment?: ModelStringInput | null,
+  createdAt?: ModelStringInput | null,
+  updatedAt?: ModelStringInput | null,
+  and?: Array< ModelCommentsFilterInput | null > | null,
+  or?: Array< ModelCommentsFilterInput | null > | null,
+  not?: ModelCommentsFilterInput | null,
+};
+
+export type ModelIDInput = {
+  ne?: string | null,
+  eq?: string | null,
+  le?: string | null,
+  lt?: string | null,
+  ge?: string | null,
+  gt?: string | null,
+  contains?: string | null,
+  notContains?: string | null,
+  between?: Array< string | null > | null,
+  beginsWith?: string | null,
+  attributeExists?: boolean | null,
+  attributeType?: ModelAttributeTypes | null,
+  size?: ModelSizeInput | null,
+};
+
+export type ModelCommentsConnection = {
+  __typename: "ModelCommentsConnection",
+  items:  Array<Comments | null >,
+  nextToken?: string | null,
+};
+
+export type ModelProjectFilterInput = {
+  id?: ModelIDInput | null,
+  userId?: ModelStringInput | null,
+  name?: ModelStringInput | null,
+  description?: ModelStringInput | null,
+  content?: ModelStringInput | null,
+  createdAt?: ModelStringInput | null,
+  updatedAt?: ModelStringInput | null,
+  and?: Array< ModelProjectFilterInput | null > | null,
+  or?: Array< ModelProjectFilterInput | null > | null,
+  not?: ModelProjectFilterInput | null,
+};
+
+export type ModelProjectConnection = {
+  __typename: "ModelProjectConnection",
+  items:  Array<Project | null >,
+  nextToken?: string | null,
+};
+
+export type ModelSubscriptionCommentsFilterInput = {
+  id?: ModelSubscriptionIDInput | null,
+  comment?: ModelSubscriptionStringInput | null,
+  createdAt?: ModelSubscriptionStringInput | null,
+  updatedAt?: ModelSubscriptionStringInput | null,
+  and?: Array< ModelSubscriptionCommentsFilterInput | null > | null,
+  or?: Array< ModelSubscriptionCommentsFilterInput | null > | null,
+};
+
+export type ModelSubscriptionIDInput = {
+  ne?: string | null,
+  eq?: string | null,
+  le?: string | null,
+  lt?: string | null,
+  ge?: string | null,
+  gt?: string | null,
+  contains?: string | null,
+  notContains?: string | null,
+  between?: Array< string | null > | null,
+  beginsWith?: string | null,
+  in?: Array< string | null > | null,
+  notIn?: Array< string | null > | null,
+};
+
+export type ModelSubscriptionStringInput = {
+  ne?: string | null,
+  eq?: string | null,
+  le?: string | null,
+  lt?: string | null,
+  ge?: string | null,
+  gt?: string | null,
+  contains?: string | null,
+  notContains?: string | null,
+  between?: Array< string | null > | null,
+  beginsWith?: string | null,
+  in?: Array< string | null > | null,
+  notIn?: Array< string | null > | null,
+};
+
+export type ModelSubscriptionProjectFilterInput = {
+  id?: ModelSubscriptionIDInput | null,
+  userId?: ModelSubscriptionStringInput | null,
+  name?: ModelSubscriptionStringInput | null,
+  description?: ModelSubscriptionStringInput | null,
+  content?: ModelSubscriptionStringInput | null,
+  createdAt?: ModelSubscriptionStringInput | null,
+  updatedAt?: ModelSubscriptionStringInput | null,
+  and?: Array< ModelSubscriptionProjectFilterInput | null > | null,
+  or?: Array< ModelSubscriptionProjectFilterInput | null > | null,
+};
+
+export type CreateCommentsMutationVariables = {
+  input: CreateCommentsInput,
+  condition?: ModelCommentsConditionInput | null,
+};
+
+export type CreateCommentsMutation = {
+  createComments?:  {
+    __typename: "Comments",
+    id: string,
+    comment?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type UpdateCommentsMutationVariables = {
+  input: UpdateCommentsInput,
+  condition?: ModelCommentsConditionInput | null,
+};
+
+export type UpdateCommentsMutation = {
+  updateComments?:  {
+    __typename: "Comments",
+    id: string,
+    comment?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type DeleteCommentsMutationVariables = {
+  input: DeleteCommentsInput,
+  condition?: ModelCommentsConditionInput | null,
+};
+
+export type DeleteCommentsMutation = {
+  deleteComments?:  {
+    __typename: "Comments",
+    id: string,
+    comment?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type CreateProjectMutationVariables = {
+  input: CreateProjectInput,
+  condition?: ModelProjectConditionInput | null,
+};
+
+export type CreateProjectMutation = {
+  createProject?:  {
+    __typename: "Project",
+    id: string,
+    userId: string,
+    name: string,
+    description?: string | null,
+    content?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type UpdateProjectMutationVariables = {
+  input: UpdateProjectInput,
+  condition?: ModelProjectConditionInput | null,
+};
+
+export type UpdateProjectMutation = {
+  updateProject?:  {
+    __typename: "Project",
+    id: string,
+    userId: string,
+    name: string,
+    description?: string | null,
+    content?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type DeleteProjectMutationVariables = {
+  input: DeleteProjectInput,
+  condition?: ModelProjectConditionInput | null,
+};
+
+export type DeleteProjectMutation = {
+  deleteProject?:  {
+    __typename: "Project",
+    id: string,
+    userId: string,
+    name: string,
+    description?: string | null,
+    content?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type GetCommentsQueryVariables = {
+  id: string,
+};
+
+export type GetCommentsQuery = {
+  getComments?:  {
+    __typename: "Comments",
+    id: string,
+    comment?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type ListCommentsQueryVariables = {
+  filter?: ModelCommentsFilterInput | null,
+  limit?: number | null,
+  nextToken?: string | null,
+};
+
+export type ListCommentsQuery = {
+  listComments?:  {
+    __typename: "ModelCommentsConnection",
+    items:  Array< {
+      __typename: "Comments",
+      id: string,
+      comment?: string | null,
+      createdAt: string,
+      updatedAt: string,
+    } | null >,
+    nextToken?: string | null,
+  } | null,
+};
+
+export type GetProjectQueryVariables = {
+  id: string,
+};
+
+export type GetProjectQuery = {
+  getProject?:  {
+    __typename: "Project",
+    id: string,
+    userId: string,
+    name: string,
+    description?: string | null,
+    content?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type ListProjectsQueryVariables = {
+  filter?: ModelProjectFilterInput | null,
+  limit?: number | null,
+  nextToken?: string | null,
+};
+
+export type ListProjectsQuery = {
+  listProjects?:  {
+    __typename: "ModelProjectConnection",
+    items:  Array< {
+      __typename: "Project",
+      id: string,
+      userId: string,
+      name: string,
+      description?: string | null,
+      content?: string | null,
+      createdAt: string,
+      updatedAt: string,
+    } | null >,
+    nextToken?: string | null,
+  } | null,
+};
+
+export type OnCreateCommentsSubscriptionVariables = {
+  filter?: ModelSubscriptionCommentsFilterInput | null,
+};
+
+export type OnCreateCommentsSubscription = {
+  onCreateComments?:  {
+    __typename: "Comments",
+    id: string,
+    comment?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type OnUpdateCommentsSubscriptionVariables = {
+  filter?: ModelSubscriptionCommentsFilterInput | null,
+};
+
+export type OnUpdateCommentsSubscription = {
+  onUpdateComments?:  {
+    __typename: "Comments",
+    id: string,
+    comment?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type OnDeleteCommentsSubscriptionVariables = {
+  filter?: ModelSubscriptionCommentsFilterInput | null,
+};
+
+export type OnDeleteCommentsSubscription = {
+  onDeleteComments?:  {
+    __typename: "Comments",
+    id: string,
+    comment?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type OnCreateProjectSubscriptionVariables = {
+  filter?: ModelSubscriptionProjectFilterInput | null,
+};
+
+export type OnCreateProjectSubscription = {
+  onCreateProject?:  {
+    __typename: "Project",
+    id: string,
+    userId: string,
+    name: string,
+    description?: string | null,
+    content?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type OnUpdateProjectSubscriptionVariables = {
+  filter?: ModelSubscriptionProjectFilterInput | null,
+};
+
+export type OnUpdateProjectSubscription = {
+  onUpdateProject?:  {
+    __typename: "Project",
+    id: string,
+    userId: string,
+    name: string,
+    description?: string | null,
+    content?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type OnDeleteProjectSubscriptionVariables = {
+  filter?: ModelSubscriptionProjectFilterInput | null,
+};
+
+export type OnDeleteProjectSubscription = {
+  onDeleteProject?:  {
+    __typename: "Project",
+    id: string,
+    userId: string,
+    name: string,
+    description?: string | null,
+    content?: string | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
