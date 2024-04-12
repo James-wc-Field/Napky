ffc1737ce12455798dc7f2ba7458340ba91f5a3c
Hamilton Hardy <hamilton.j.hardy@gmail.com>
2024-04-12 10:12:40 -0600

    still not working

--- src/app/discover/[projectID]/project.tsx
+++ src/app/discover/[projectID]/project.tsx
@@ -53,7 +53,6 @@ export default function Project() {
         document.addEventListener("mousemove", handleMouseMove);
         document.addEventListener("mouseup", handleMouseUp);
         return () => {
-            // Cleanup event listeners when component unmounts
             document.removeEventListener("mousemove", handleMouseMove);
             document.removeEventListener("mouseup", handleMouseUp);
         };
@@ -61,6 +60,36 @@ export default function Project() {
     const BASE_SIZE = 24;
     return (
         <>
+            <svg
+                className="absolute w-full h-full top-15 left-0 z-0"
+                style={{
+                    visibility: zoomLevel < 0.5 ? "hidden" : "visible",
+                }}
+            >
+                <pattern
+                    id="background"
+                    x={scrollLeft % (BASE_SIZE * zoomLevel)}
+                    y={scrollTop % (BASE_SIZE * zoomLevel)}
+                    width={BASE_SIZE * zoomLevel}
+                    height={BASE_SIZE * zoomLevel}
+                    patternUnits="userSpaceOnUse"
+                    patternTransform={`translate(${zoomLevel}, ${zoomLevel})`}
+                >
+                    <circle
+                        cx={zoomLevel}
+                        cy={zoomLevel}
+                        r={zoomLevel}
+                        fill="#91919a"
+                    ></circle>
+                </pattern>
+                <rect
+                    x="0"
+                    y="0"
+                    width="100%"
+                    height="100%"
+                    fill="url(#background)"
+                ></rect>
+            </svg>
             <div
                 id="canvas-renderer"
                 className="absolute w-full h-full top-0 left-0"
@@ -102,36 +131,6 @@ export default function Project() {
                 </Button>
             </Card>
             {/* <MiniMap /> */}
-            <svg
-                className="absolute w-full h-full top-0 left-0"
-                style={{
-                    visibility: zoomLevel < 0.5 ? "hidden" : "visible",
-                }}
-            >
-                <pattern
-                    id="background"
-                    x={scrollLeft % (BASE_SIZE * zoomLevel)}
-                    y={scrollTop % (BASE_SIZE * zoomLevel)}
-                    width={BASE_SIZE * zoomLevel}
-                    height={BASE_SIZE * zoomLevel}
-                    patternUnits="userSpaceOnUse"
-                    patternTransform={`translate(${zoomLevel}, ${zoomLevel})`}
-                >
-                    <circle
-                        cx={zoomLevel}
-                        cy={zoomLevel}
-                        r={zoomLevel}
-                        fill="#91919a"
-                    ></circle>
-                </pattern>
-                <rect
-                    x="0"
-                    y="0"
-                    width="100%"
-                    height="100%"
-                    fill="url(#background)"
-                ></rect>
-            </svg>
         </>
     )
 }
@@ -154,7 +153,7 @@ function CanvasElementWrapper({
 
 
     return (
-        <div style={style}>
+        <div style={style} className="z-0">
             <div className="relative">
                 <CanvasElement elementInstance={element} />
             </div>
