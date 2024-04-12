e6051a0790f923d9d07fd18b708f0d2cdd22716c
Hamilton Hardy <hamilton.j.hardy@gmail.com>
2024-04-12 13:45:18 -0600

    remove dup nav bar

--- src/app/discover/[projectID]/layout.tsx
+++ src/app/discover/[projectID]/layout.tsx
@@ -1,27 +1,11 @@
 import { ReactNode } from "react";
 import { DiscoverProjectStoreProvider } from "./storeProvider";
-import { NavigationBar } from "@/components/NavigationBar";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
-        <div className="flex flex-col min-h-screen max-h-screen min-w-full">
-            <main className="flex grow">
-                <div className="w-full mx-auto overflow-hidden">
-                    <NavigationBar>
-                        <div style={{ marginLeft: "auto" }}>
-                            <Button variant={"ghost"} className="text-lg" asChild>
-                                <Link href={"/discover"}>Discover</Link>
-                            </Button>
-                        </div>
-                    </NavigationBar>
-                    <DiscoverProjectStoreProvider>
-                        {children}
-                    </DiscoverProjectStoreProvider>
-                </div>
-            </main>
-        </div>
+        <DiscoverProjectStoreProvider>
+            {children}
+        </DiscoverProjectStoreProvider>
     )
 
 }
