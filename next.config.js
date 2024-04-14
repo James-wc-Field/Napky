3f8a83c63224c1d461631173daf2257b735833c0
cmax <a02335780@usu.edu>
2024-04-13 22:46:33 -0600

    Probably don't want unknown images going through next optimizer

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'korkbof3b8f9ace4cc4641b7954058899ccc71145707-dev.s3.us-west-1.amazonaws.com',
-                port: "",
-                pathname: '/**',
-
-            }
-        ]
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
