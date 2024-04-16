e4c6acc746808c415f6b15ddd5a7d10f5864ef7f
Hamilton Hardy <hamilton.j.hardy@gmail.com>
2024-04-16 00:05:49 -0600

    cleanup complete

--- src/app/project/[projectID]/utilities/index.ts
+++ src/app/project/[projectID]/utilities/index.ts
@@ -1,7 +1,3 @@
-export * from "./adjust-element-coordinates";
-export * from "./adjustment-required";
-export * from "./create-element";
 export * from "./cursor-for-position";
 export * from "./draw-element";
 export * from "./get-element-at-position";
-export * from "./resized-coordinates";
